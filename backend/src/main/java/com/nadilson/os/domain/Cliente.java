2b63718825865311979a0a3706216eea8f273064
Nadilson <nadilson@protonmail.com>
2022-06-15 03:25:54 -0300

    Criado o domain do Ordens de Serviço (parte 3)

--- backend/src/main/java/com/nadilson/os/domain/Cliente.java
+++ backend/src/main/java/com/nadilson/os/domain/Cliente.java
@@ -1,13 +1,33 @@
 package com.nadilson.os.domain;
 
-public class Cliente extends Pessoa {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Cliente extends Pessoa implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@OneToMany(mappedBy = "cliente")
+	private List<OS> lista = new ArrayList<>();
+	
 	public Cliente() {
 		super();
 	}
 
 	public Cliente(Integer id, String nome, String cpf, String telefone) {
 		super(id, nome, cpf, telefone);
+	}
+
+	public List<OS> getLista() {
+		return lista;
+	}
+
+	public void setLista(List<OS> lista) {
+		this.lista = lista;
 	}
 
 	
--- backend/src/main/java/com/nadilson/os/domain/Tecnico.java
+++ backend/src/main/java/com/nadilson/os/domain/Tecnico.java
@@ -1,13 +1,33 @@
 package com.nadilson.os.domain;
 
-public class Tecnico extends Pessoa {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Tecnico extends Pessoa implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@OneToMany(mappedBy = "tecnico")
+	private List<OS> lista = new ArrayList<>();
+	
 	public Tecnico() {
 		super();
 	}
 
 	public Tecnico(Integer id, String nome, String cpf, String telefone) {
 		super(id, nome, cpf, telefone);
+	}
+
+	public List<OS> getLista() {
+		return lista;
+	}
+
+	public void setLista(List<OS> lista) {
+		this.lista = lista;
 	}
 	
 	
