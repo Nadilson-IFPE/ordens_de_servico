5d9e64534b3d45060c93e9da3805a12403bc3cdf
Nadilson <nadilson@protonmail.com>
2022-06-18 03:26:30 -0300

    Tratamento de exceções

--- frontend/src/app/views/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ frontend/src/app/views/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { TecnicoService } from "src/app/services/tecnico.service";
+import { Tecnico } from "src/app/models/tecnico";
 
 @Component({
   selector: "app-tecnico-create",
@@ -7,11 +9,34 @@ import { Router } from "@angular/router";
   styleUrls: ["./tecnico-create.component.css"],
 })
 export class TecnicoCreateComponent implements OnInit {
-  constructor(private router: Router) {}
+  tecnico: Tecnico = {
+    id: "",
+    nome: "Ana",
+    cpf: "856.768.050-64",
+    telefone: "(00) 90000-0000",
+  };
+
+  constructor(private router: Router, private service: TecnicoService) {}
 
   ngOnInit(): void {}
 
   cancel(): void {
     this.router.navigate(["tecnicos"]);
+  }
+
+  create(): void {
+    this.service.create(this.tecnico).subscribe(
+      (resposta) => {
+        this.router.navigate(["tecnicos"]);
+        this.service.message("Técnico  criado com sucesso!");
+      },
+      (err) => {
+        console.log(err);
+        if (err.error.error.match("já cadastrado")) {
+          console.log(err.error.error);
+          this.service.message(err.error.error);
+        }
+      }
+    );
   }
 }
