a7a6a8fd03367f441d7aa55e82363f961b934fd6
Nadilson <nadilson@protonmail.com>
2022-06-18 02:27:53 -0300

    Componente create para Tecnico(Parte 2)

--- frontend/src/app/views/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ frontend/src/app/views/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-tecnico-create',
-  templateUrl: './tecnico-create.component.html',
-  styleUrls: ['./tecnico-create.component.css']
+  selector: "app-tecnico-create",
+  templateUrl: "./tecnico-create.component.html",
+  styleUrls: ["./tecnico-create.component.css"],
 })
 export class TecnicoCreateComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  cancel(): void {
+    this.router.navigate(["tecnicos"]);
   }
-
 }
