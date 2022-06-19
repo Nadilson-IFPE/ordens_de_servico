43c27aaacfeb8317ed77440f3c1a174237faa561
Nadilson <nadilson@protonmail.com>
2022-06-19 17:58:02 -0300

    Componente read para Ordens de Serviço (Parte 1)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -32,6 +32,7 @@ import { ClienteReadComponent } from "./views/components/cliente/cliente-read/cl
 import { ClienteCreateComponent } from "./views/components/cliente/cliente-create/cliente-create.component";
 import { ClienteUpdateComponent } from "./views/components/cliente/cliente-update/cliente-update.component";
 import { ClienteDeleteComponent } from './views/components/cliente/cliente-delete/cliente-delete.component';
+import { OsReadComponent } from './views/components/os/os-read/os-read.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -47,6 +48,7 @@ import { ClienteDeleteComponent } from './views/components/cliente/cliente-delet
     ClienteCreateComponent,
     ClienteUpdateComponent,
     ClienteDeleteComponent,
+    OsReadComponent,
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/views/components/os/os-read/os-read.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { AfterViewInit } from "@angular/core";
+import { MatTableDataSource } from "@angular/material/table";
+import { OS } from "./../../../../models/os";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatSort } from "@angular/material/sort";
+import { OsService } from "./../../../../services/os.service";
+import { Router } from "@angular/router";
+
+@Component({
+  selector: "app-os-read",
+  templateUrl: "./os-read.component.html",
+  styleUrls: ["./os-read.component.css"],
+})
+export class OsReadComponent implements AfterViewInit {
+  listaDeOS: OS[] = [];
+
+  displayedColumns: string[] = [
+    "tecnico",
+    "cliente",
+    "abertura",
+    "fechamento",
+    "prioridade",
+    "status",
+    "action",
+  ];
+  dataSource = new MatTableDataSource<OS>(this.listaDeOS);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) ordenar!: MatSort;
+
+  constructor(private service: OsService, private router: Router) {}
+
+  ngAfterViewInit() {
+    this.findAll();
+
+    this.paginator._intl.itemsPerPageLabel = "Itens por página";
+    this.paginator._intl.firstPageLabel = "Página inicial";
+    this.paginator._intl.lastPageLabel = "Última página";
+    this.paginator._intl.previousPageLabel = "Página anterior";
+    this.paginator._intl.nextPageLabel = "Próxima página";
+    this.paginator._intl.getRangeLabel = (
+      page: number,
+      pageSize: number,
+      length: number
+    ) => {
+      if (length === 0 || pageSize === 0) {
+        return `0 de ${length}`;
+      }
+      if (pageSize >= length) {
+        return `1 de 1`;
+      }
+      length = Math.max(length, 0);
+      const startIndex = page * pageSize;
+
+      startIndex < length
+        ? Math.min(startIndex + pageSize, length)
+        : startIndex + pageSize;
+      return `${startIndex + 1} de ${length}`;
+    };
+  }
+
+  findAll(): void {
+    this.service.findAll().subscribe((resposta) => {
+      this.listaDeOS = resposta;
+      this.dataSource = new MatTableDataSource<OS>(this.listaDeOS);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.ordenar;
+    });
+  }
+
+  navigateToCreate(): void {
+    this.router.navigate(["/os/create"]);
+  }
+}
