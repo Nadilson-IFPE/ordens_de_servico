a50b6828a7b0cb5188a421b3f3bcbc9f2bfb5c0d
Nadilson <nadilson@protonmail.com>
2022-06-19 17:31:17 -0300

    Implementando o serviço para Ordem de Serviço (front)

---
+++ frontend/src/app/services/os.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from "@angular/core";
+import { environment } from "src/environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { Observable } from "rxjs";
+import { OS } from "./../models/os";
+
+@Injectable({
+  providedIn: "root",
+})
+export class OsService {
+  baseURL: String = environment.baseURL;
+
+  constructor(private http: HttpClient, private snack: MatSnackBar) {}
+
+  findAll(): Observable<OS[]> {
+    const url = this.baseURL + "/os";
+    return this.http.get<OS[]>(url);
+  }
+
+  findById(id: any): Observable<OS> {
+    const url = `${this.baseURL}/os/${id}`;
+    return this.http.get<OS>(url);
+  }
+
+  create(os: OS): Observable<OS> {
+    const url = this.baseURL + "/os";
+    return this.http.post<OS>(url, os);
+  }
+
+  update(os: OS): Observable<OS> {
+    const url = `${this.baseURL}/os/${os.id}`;
+    return this.http.put<OS>(url, os);
+  }
+
+  delete(id: any): Observable<void> {
+    const url = `${this.baseURL}/os/${id}`;
+    return this.http.delete<void>(url);
+  }
+
+  message(msg: String): void {
+    this.snack.open(`${msg}`, "OK", {
+      horizontalPosition: "end",
+      verticalPosition: "top",
+      duration: 4000,
+    });
+  }
+}
