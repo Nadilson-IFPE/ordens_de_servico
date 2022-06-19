7f00427ca724ec691dfc853cfa303ce56afa560f
Nadilson <nadilson@protonmail.com>
2022-06-19 16:16:36 -0300

    Componente create para Cliente

--- frontend/src/app/services/cliente.service.ts
+++ frontend/src/app/services/cliente.service.ts
@@ -17,4 +17,17 @@ export class ClienteService {
     const url = this.baseURL + "/clientes";
     return this.http.get<Cliente[]>(url);
   }
+
+  create(cliente: Cliente): Observable<Cliente> {
+    const url = this.baseURL + "/clientes";
+    return this.http.post<Cliente>(url, cliente);
+  }
+
+  message(msg: String): void {
+    this.snack.open(`${msg}`, "OK", {
+      horizontalPosition: "end",
+      verticalPosition: "top",
+      duration: 4000,
+    });
+  }
 }
