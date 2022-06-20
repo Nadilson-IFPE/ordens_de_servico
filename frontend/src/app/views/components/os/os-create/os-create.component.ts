f6aceb6d59172035a57dd962a83239b68bf6f8bf
Nadilson <nadilson@protonmail.com>
2022-06-19 22:06:28 -0300

    Componente create para Ordem de Serviço (Parte2)

--- frontend/src/app/views/components/os/os-create/os-create.component.ts
+++ frontend/src/app/views/components/os/os-create/os-create.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { TecnicoService } from "src/app/services/tecnico.service";
+import { Tecnico } from "src/app/models/tecnico";
+import { ClienteService } from "src/app/services/cliente.service";
+import { Cliente } from "./../../../../models/cliente";
 
 @Component({
-  selector: 'app-os-create',
-  templateUrl: './os-create.component.html',
-  styleUrls: ['./os-create.component.css']
+  selector: "app-os-create",
+  templateUrl: "./os-create.component.html",
+  styleUrls: ["./os-create.component.css"],
 })
 export class OsCreateComponent implements OnInit {
+  selected = "";
 
-  constructor() { }
+  tecnicos: Tecnico[] = [];
+  clientes: Cliente[] = [];
+
+  constructor(
+    private tecnicoService: TecnicoService,
+    private clienteService: ClienteService
+  ) {}
 
   ngOnInit(): void {
+    this.listarTecnicos();
+    this.listarClientes();
   }
 
+  listarTecnicos(): void {
+    this.tecnicoService.findAll().subscribe((resposta) => {
+      this.tecnicos = resposta;
+    });
+  }
+
+  listarClientes(): void {
+    this.clienteService.findAll().subscribe((resposta) => {
+      this.clientes = resposta;
+    });
+  }
 }
